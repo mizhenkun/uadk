421f86350416d656f4d3b7851ac5110b277c4048
Zhou Wang <wangzhou1@hisilicon.com>
2020-09-19 11:31:29 +0800

    wd: remove common codes in cipher and digest
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- drv/hisi_sec.c
+++ drv/hisi_sec.c
@@ -125,7 +125,7 @@ enum sec_cipher_dir {
 };
 
 struct hisi_sec_ctx {
-	struct wd_ctx_config config;
+	struct wd_ctx_config_internal config;
 };
 
 struct hisi_sec_sqe_type2 {
@@ -288,7 +288,7 @@ static void update_iv(struct wd_cipher_msg *msg)
 {
 }
 
-int hisi_sec_init(struct wd_ctx_config *config, void *priv)
+int hisi_sec_init(struct wd_ctx_config_internal *config, void *priv)
 {
 	struct hisi_sec_ctx *sec_ctx = priv;
 	struct hisi_qm_priv qm_priv;
@@ -304,7 +304,7 @@ int hisi_sec_init(struct wd_ctx_config *config, void *priv)
 		if (!h_qp)
 			goto out;
 	}
-	memcpy(&sec_ctx->config, config, sizeof(struct wd_ctx_config));
+	memcpy(&sec_ctx->config, config, sizeof(struct wd_ctx_config_internal));
 	return 0;
 out:
 	for (; i >= 0 ; i--) {
@@ -322,7 +322,7 @@ void hisi_sec_exit(void *priv)
 	}
 
 	struct hisi_sec_ctx *sec_ctx = priv;
-	struct wd_ctx_config *config = &sec_ctx->config;
+	struct wd_ctx_config_internal *config = &sec_ctx->config;
 	handle_t h_qp;
 	int i;
 
